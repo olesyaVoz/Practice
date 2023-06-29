00509fe6cd1576d3522194dfdb6e96dae920ddb9
Olesya Voznesenskaya <odv1@tpu.ru>
2023-06-29 22:06:51 +0700

    Pt_2_3: Full task
    I wrote a program that checks if a number is an Armstrong number.

---
+++ Pt_2_3.py
@@ -0,0 +1,10 @@
+num = (int(input("Введите число: ")))
+nums = str(num)
+sum = 0
+n = len(nums)
+for digit in nums:
+      sum += int(digit) ** n
+if num == sum:
+    print(num, "является числом Армстронга")
+else:
+    print(num, "не является числом Армстронга")
