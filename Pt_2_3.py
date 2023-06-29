eeefa4a30710175ae81aa3435c7bb596d72b3cb7
Olesya Voznesenskaya <odv1@tpu.ru>
2023-06-29 22:48:08 +0700

    Pt_2_3: Changed formatting

--- Pt_2_3.py
+++ Pt_2_3.py
@@ -3,7 +3,7 @@ nums = str(num)
 sum = 0
 n = len(nums)
 for digit in nums:
-      sum += int(digit) ** n
+    sum += int(digit) ** n
 if num == sum:
     print(num, "является числом Армстронга")
 else:
