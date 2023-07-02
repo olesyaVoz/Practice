f389752a40fb773f2d5f9090f0d08844113ad0e3
Olesya Voznesenskaya <odv1@tpu.ru>
2023-07-02 17:47:43 +0700

    Pt_3_7: Full task
    I wrote a program that creates a dictionary,
    where the keys are letters from the string,
    and the values are True if the letter is vowel,
    and False if the letter is consonant

---
+++ Pt_3_7.py
@@ -0,0 +1,5 @@
+vowels = 'уеёыаоэяию'
+consonants = 'йцкнгшщзхъфвпрлджчсмтьб'
+s = input("Введите строку: ")
+letter_dict = {letter: True if letter.lower() in vowels else False for letter in s if letter.isalpha()}
+print(letter_dict)
