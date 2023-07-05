a68aca94e4b5a08d129819ca5c33cd2d6f737ccd
Olesya Voznesenskaya <odv1@tpu.ru>
2023-07-05 23:17:09 +0700

    Pt_2_5: Full task
    I wrote a program that plays horoscope with the user.
    The program must ask the user whether it is heads or tails.
    If the user enters 0, he chooses heads,
    1 - tails, any other number is the end of the game.
    The program keeps a record of winnings and
    of wins and losses after each round.
    After three consecutive losses the game ends.

---
+++ Pt_2_5.py
@@ -0,0 +1,26 @@
+import random
+
+game = ["0", "1"]
+temp = random.choice(game)
+sum = 0
+wins = 0
+losses = 0
+while sum < 3:
+    s = str(input("Выберете орел - 0 или решку - 1:"))
+    if temp == s:
+        print("Ты угадал!")
+        wins += 1
+        sum = 0
+    elif s not in temp:
+        print("Конец игры")
+        break
+    else:
+        losses += 1
+        sum += 1
+        print("Ты не угадал!")
+
+    print("Счет: побед -", wins, ", поражений —", losses)
+else:
+    print("Игра завершена")
+
+
