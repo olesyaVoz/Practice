e6c92bb1d0a793ffc8709ba87db98c5d14b0f13c
Olesya Voznesenskaya <odv1@tpu.ru>
2023-07-11 18:44:26 +0700

    Pt_3_2: Full task
    I wrote a lambda function that returns a list
    of all Fibonacci numbers up to a given number

--- Pt_3_2.py
+++ Pt_3_2.py
@@ -1,12 +1,3 @@
 n = int(input("Введите число: "))
-def fib(n):
-    fib_list = []
-    a, b = 0, 1
-    while a < n:
-        fib_list.append(a)
-        c = a
-        a = b
-        b = c + a
-    return fib_list
-result = fib(n)
-print("Последовательность чисел Фиббоначи, не привышающих ", n, ":",  result)
+fib = lambda n, a=0, b=1: [a] + fib(n, b, a + b) if b <= n else [a]
+print("Последовательность чисел Фиббоначи, не привышающих ", n, ":",  fib(n))
