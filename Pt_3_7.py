d4997aff79c120342cf24cd167bc01a6dfca04b6
olesyaVoz <odv1@tpu.ru>
2023-07-17 19:04:19 +0300

    Pt_3_7: change by pep8

--- Pt_3_7.py
+++ Pt_3_7.py
@@ -1,5 +1,6 @@
 vowels = 'уеёыаоэяию'
 consonants = 'йцкнгшщзхъфвпрлджчсмтьб'
 s = input("Введите строку: ")
-letter_dict = {letter: True if letter.lower() in vowels else False for letter in s if letter.isalpha()}
+letter_dict = {letter: True if letter.lower() in vowels 
+               else False for letter in s if letter.isalpha()}
 print(letter_dict)
